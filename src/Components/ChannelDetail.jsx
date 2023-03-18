3f002c9b7cb4afd362fa314c644437109c1259c4
mohammedWasik <mwasik5050@google.com>
2023-03-18 20:47:24 +0600

    removed unused imports

--- src/Components/ChannelDetail.jsx
+++ src/Components/ChannelDetail.jsx
@@ -1,4 +1,3 @@
-import { SmartButton } from "@mui/icons-material";
 import { Box } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
